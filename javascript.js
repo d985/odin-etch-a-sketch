4444bd5dd84e9007cdc3a0688f42d42fbd000129
Huyson Lam <5705943+lycheejello@users.noreply.github.com>
2024-02-07 21:34:18 -0800

    quick and dirty up to step3

--- javascript.js
+++ javascript.js
@@ -1,13 +1,28 @@
 const container = document.querySelector('#container');
-const divWidth = document.body.clientWidth/4;
+
+const divWidth = window.innerWidth / 4 - 8;
+const divHeight = window.innerHeight / 4 - 8;
+
+
 console.log(window.innerWidth);
-console.log(document.body.clientWidth);
+console.log(divHeight);
+//console.log(document.body.clientWidth);
+
 
 for (let i = 0; i < 16; i++) {
     const div = document.createElement('div');
-    div.classList.add('grid');
+    div.classList.add('pixel');
+    div.setAttribute('class', 'bg-1')
     div.style.width = `${divWidth}px`;
-    div.style.height = `180px`;
+    div.style.height = `${divHeight}px`;
     div.style.borderStyle = 'solid';
+
+    div.onmouseenter = function() {
+        div.removeAttribute('class')
+        div.setAttribute('class', 'bg-0')
+    }
+
     container.appendChild(div);
 }
+
+
