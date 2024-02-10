31ff40e27d6961d552b230fa247ba262c8f7a2cc
derrick <61602304+d985@users.noreply.github.com>
2024-02-09 22:13:58 -0800

    Kind of working version, but spacing is off at higher number (elements per side > ~60)

--- javascript.js
+++ javascript.js
@@ -1,13 +1,32 @@
 const container = document.querySelector('#container');
-const divWidth = document.body.clientWidth/4;
-console.log(window.innerWidth);
-console.log(document.body.clientWidth);
+const containerHeight = window.innerHeight - 18 - 37.2; // 37.2 is size of button + header div padding; 18 accounts for borders + ???
+container.style.height = `${containerHeight}px`;
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    div.style.width = `${divWidth}px`;
-    div.style.height = `180px`;
-    div.style.borderStyle = 'solid';
-    container.appendChild(div);
+function createGrid(elementsPerSide) {
+    container.innerHTML = "";
+    let divWidth =  document.body.clientWidth/elementsPerSide - 4;
+
+    for (let i = 0; i < elementsPerSide**2; i++) {        
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        div.style.width = `${divWidth}px`;
+        div.style.borderStyle = 'solid';
+        div.style.borderWidth = '2px';
+        div.textContent = i;
+        
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "black";
+        });
+        
+        container.appendChild(div);
+    }
 }
+
+createGrid(4);
+
+const resizeButton = document.querySelector('#resizeButton');
+resizeButton.addEventListener("click", () => {
+    let numElements = prompt("Enter the number of elements per side", "10");
+    if (numElements > 100) { numElements = 100 };
+    createGrid(numElements);
+});
